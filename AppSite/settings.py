908a2c90062c5e4c6504487b4b6f1876447ab93b
Favas abdul nassar <favasabdulnassar@gmail.com>
2025-02-20 03:22:37 +0530

    Added allowed host

--- AppSite/settings.py
+++ AppSite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','django-insecure-GENERATED_NEW_KEY_HE
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://appsite-backend.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition
